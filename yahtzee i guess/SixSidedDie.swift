bb0cc7c7f1420a385122661ef3e6d8f07d3f8dce
Joey Ehlert <joey@ehlert.org>
2016-05-24 13:33:03 -0500

    added SixSidedDie

---
+++ yahtzee i guess/SixSidedDie.swift
@@ -0,0 +1,21 @@
+//
+//  SixSidedDie.swift
+//  yahtzee i guess
+//
+//  Created by Joseph Ehlert on 5/23/16.
+//  Copyright © 2016 Joselliam. All rights reserved.
+//
+
+import Darwin
+
+class SixSidedDie {
+    var side: Int
+    
+    init() {
+        side = Int(arc4random_uniform(6) + 1)
+    }
+    
+    func roll() -> Int {
+        side = Int(arc4random_uniform(6) + 1)
+    }
+}
